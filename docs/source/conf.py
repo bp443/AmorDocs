f029ef866175c650d5a0141bf9718f1ccbd27b37
bp443 <57727586+bp443@users.noreply.github.com>
2023-11-22 01:33:16 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AmorFo'
+copyright = '2023'
+author = 'Balazs Pota'
 
 release = '0.1'
 version = '0.1.0'
